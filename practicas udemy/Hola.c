6a1964af854814507a42dd18768ed0b1e85cd58a
Mateos20013 <mateo.sotomayor@udla.edu.ec>
2023-07-04 13:46:39 -0500

    first commit

---
+++ practicas udemy/Hola.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+    printf("Hola mundo.");
+    return 0;
+}
+
+//Certificado del curso: https://udla.udemy.com/certificate/UC-16646d80-6241-4a0d-8b00-49246781e065/?utm_source=sendgrid.com&utm_medium=email&utm_campaign=email
